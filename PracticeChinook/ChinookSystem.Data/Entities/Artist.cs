d824bc33ace80942e51dee6e6a8fa8177169e3fe
Michael Mattai <mmattai1@studentmail.nait.ca>
2016-09-26 21:11:39 -0600

    Entity setup beginning

---
+++ PracticeChinook/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    // [Table("sqltablename")]
+    [Table("Artists")]
+    public class Artist
+    {
+        // PKey annotation is optional
+        // Default assumes identity SQL table
+        // Default assumes property name ends with ID or Id and SQL PKey ends the same
+        // Otherwise [Key] is needed
+        // Non-identity PKeys, compound PKeys, PKeys not ending in ID or Id
+
+        // Class properties map to SQL table attributes
+        // Properties should be named the same as table attributes
+        // Properties should be in the same order as table attributes for ease of maintenance
+        [Key]
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
