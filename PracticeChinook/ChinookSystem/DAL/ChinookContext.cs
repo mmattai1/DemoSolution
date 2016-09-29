5ba157136c15e1ba4673e95154f6f1ef94c4adde
Michael Mattai <mmattai1@studentmail.nait.ca>
2016-09-28 20:19:37 -0600

    Setup of DbContext and web connection strings

---
+++ PracticeChinook/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using ChinookSystem.Data.Entities;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    // Internal for security reasons
+    // Access restricted to within the component library
+    // Inherit DbContext for EntityFramework (System.Data.Entity)
+    internal class ChinookContext : DbContext
+    {
+        // Pass the connection string to the DbContext using the :base() initializer
+        public ChinookContext() : base("ChinookDB")
+        {
+
+        }
+
+        // Setup DbSet properties
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<MediaType> MediaTypes { get; set; }
+    }
+}
