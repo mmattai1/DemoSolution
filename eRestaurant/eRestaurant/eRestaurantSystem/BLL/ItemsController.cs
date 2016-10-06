587d9865cd4860a1116706d3af407ae5f35f749f
Michael Mattai <mmattai1@studentmail.nait.ca>
2016-10-05 20:10:35 -0600

    Setup DAL & DB connections

---
+++ eRestaurant/eRestaurant/eRestaurantSystem/BLL/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel;            // ODS
+using eRestaurantSystem.DAL;            // Context class
+using eRestaurantSystem.Data.Entities;  // Entity classes
+using eRestaurantSystem.Data;     // POCOs classes
+#endregion
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class ItemsController
+    {
+
+    }
+}
---
+++ eRestaurant/eRestaurant/eRestaurantSystem/DAL/eRestaurantContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.Data.Entities;
+using System.Data.Entity;
+#endregion
+
+namespace eRestaurantSystem.DAL
+{
+    internal class eRestaurantContext : DbContext
+    {
+        public eRestaurantContext() : base("eRestaurantDB")
+        {
+
+        }
+
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<BillItem> BillItems { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<MenuCategory> MenuCategories { get; set; }
+    }
+}
