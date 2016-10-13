2adc435df227b275742cf48401d9571f7159b9e9
Michael Mattai <mmattai1@studentmail.nait.ca>
2016-10-12 20:04:23 -0600

    Added RoleManager

---
+++ PracticeChinook/ChinookSystem/Security/RoleManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity.EntityFramework;    // UserStore, ApplicationDbContext
+using Microsoft.AspNet.Identity;                    // UserManager
+#endregion
+
+namespace ChinookSystem.Security
+{
+    public class RoleManager : RoleManager<IdentityRole>
+    {
+        public RoleManager() : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+
+        }
+
+    }
+}
