98139ca855e176da3a010420fccbd7b72ea24321
Michael Mattai <mmattai1@studentmail.nait.ca>
2016-10-12 20:22:53 -0600

    Add ApplicationUser unique tracking properties

--- PracticeChinook/ChinookSystem/Security/ApplicationUser.cs
+++ PracticeChinook/ChinookSystem/Security/ApplicationUser.cs
@@ -13,5 +13,7 @@ namespace ChinookSystem.Security
     // You can add User data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public int? EmployeeId { get; set; }
+        public int? CustomerId { get; set; }
     }
 }
