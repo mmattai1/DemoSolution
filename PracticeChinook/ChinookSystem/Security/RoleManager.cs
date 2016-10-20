6d20d79b65a09140a54fd88240a4f322253840e9
Michael Mattai <mmattai1@studentmail.nait.ca>
2016-10-19 19:42:41 -0600

    Create Select, Insert, and Delete role methods for controller

--- PracticeChinook/ChinookSystem/Security/RoleManager.cs
+++ PracticeChinook/ChinookSystem/Security/RoleManager.cs
@@ -7,16 +7,18 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using Microsoft.AspNet.Identity.EntityFramework;    // UserStore, ApplicationDbContext
 using Microsoft.AspNet.Identity;                    // UserManager
+using System.ComponentModel;
 #endregion
 
 namespace ChinookSystem.Security
 {
+    [DataObject]
     public class RoleManager : RoleManager<IdentityRole>
     {
         public RoleManager() : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
         {
 
-        }
+        } //eom
 
         public void AddChinookRoles()
         {
@@ -30,6 +32,44 @@ namespace ChinookSystem.Security
                     this.Create(new IdentityRole(roleName));
                 }
             }
-        }
-    }
-}
+        } //eom
+
+        // Read
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<RoleProfile> ListAllRoles()
+        {
+            var usermgr = new UserManager();
+
+            // At points one requires data to be in memory for use by other queries
+            // Here the data from the property in RoleManager needs to be brought into memory for use by my Linq query
+            var results = from role in Roles.ToList()
+                          select new RoleProfile()
+                          {
+                              RoleId = role.Id,
+                              RoleName = role.Name,
+                              UserNames = role.Users.Select(r => usermgr.FindById(r.UserId).UserName)
+                          };
+
+            return results.ToList();
+        } //eom
+
+        // Insert
+        [DataObjectMethod(DataObjectMethodType.Insert, true)]
+        public void AddRole(RoleProfile role)
+        {
+            // Business logic validation
+            // New role names cannot already exist
+            if (!this.RoleExists(role.RoleName))
+            {
+                this.Create(new IdentityRole(role.RoleName));
+            }
+        } //eom
+
+        // Delete
+        [DataObjectMethod(DataObjectMethodType.Delete, true)]
+        public void RemoveRole(RoleProfile role)
+        {
+            this.Delete(this.FindById(role.RoleId));
+        } //eom
+    } //eoc
+} //eon
